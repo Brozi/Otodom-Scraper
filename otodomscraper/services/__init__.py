2bb60cb997c28d6d2ec52572cd69b866a9297dcc
Brozi <jasiubar@gmail.com>
2026-04-12 21:00:38 +0200

    fix: add missing imports

--- otodomscraper/services/__init__.py
+++ otodomscraper/services/__init__.py
@@ -2,3 +2,4 @@ from services.agency import AgencyService  # noqa F401
 from services.database import connect_to_database  # noqa F401
 from services.property import PropertyService  # noqa F401
 from services.discovery import RangeDiscoverer
+from services.merge import DataMerger
