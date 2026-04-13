b00abc2e5fb27312fe18eb243906687e9bf28748
Brozi <jasiubar@gmail.com>
2026-04-13 09:43:48 +0200

    fix: another attempt to fix the issue

--- otodomscraper/models/property.py
+++ otodomscraper/models/property.py
@@ -49,7 +49,7 @@ class PropertyDocument(Document):
     market_type = EnumField(MarketType, required=True)
     auction_type = EnumField(AuctionType, required=True)
     localization = EmbeddedDocumentField(LocalizationDocument, required=True)
-    construction_status = StringField(ConstructionStatus, required=True)
+    construction_status = ListField(StringField(ConstructionStatus))
     building = EmbeddedDocumentField(BuildingDocument)
     offered_by = EnumField(OfferedBy, required=True)
     estate_agency = ReferenceField("AgencyDocument", reverse_delete_rule=NULLIFY)
