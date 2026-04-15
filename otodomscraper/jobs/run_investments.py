346688978408ffdf6f5db2184d967414065f1641
Brozi <jasiubar@gmail.com>
2026-04-15 20:28:06 +0200

    fix: add excel and csv write for run_investments

--- otodomscraper/jobs/run_investments.py
+++ otodomscraper/jobs/run_investments.py
@@ -24,6 +24,13 @@ def main():
 
     crawler.process_investment_queue()
 
+    import time
+    timestamp = int(time.time())
+    crawler.to_csv_file(f"investments_{timestamp}.csv")
+    from pandas import read_csv
+    df = read_csv(f"listings_{timestamp}.csv")
+    df.to_excel(f"listings_{timestamp}.xlsx", index=False)
+
 
 if __name__ == "__main__":
     main()
